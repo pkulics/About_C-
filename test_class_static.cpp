893e2ba9b29dff64322def4b6bfafbe7c3fa062e
pkulics <lics999@126.com>
2020-01-17 11:18:43 +0800

    class static 变量用法

--- test_class_static.cpp
+++ test_class_static.cpp
@@ -6,14 +6,14 @@ class Box
 {
     public:
         Box();
-        static int niu;
+        static int niu;//声明静态变量niu 
 };
 
 
-int Box::niu = 0;
+int Box::niu = 0;//调用之前必须先在类外实例化
 Box::Box(){
-    niu ++;
-    if(niu > 5){
+    niu ++;//每初始化一个类，niu = niu + 1
+    if(niu > 5){//当niu大于5时，重置为0
         niu = 0;
     }
     cout<<"niu is:"<<niu<<endl;
